27934f505c9b41de2447adacec914c8c20d53408
Nirmal Jasmatiya <nirmal@Nirmals-MacBook-Pro.local>
2021-02-23 12:27:51 +0530

    WS Reply Channel Topic Updated

--- app/Listeners/Tweet.js
+++ app/Listeners/Tweet.js
@@ -5,8 +5,12 @@ const Ws = use("Ws");
 const Tweet = (exports = module.exports = {});
 
 Tweet.created = async (event) => {
+	console.log("===============================");
+	console.log("NEW EVENT \n", event);
+	console.log("===============================");
 	for (let index = 0; index < event.tweet_create_events.length; index++) {
 		const element = event.tweet_create_events[index];
+
 		let topic = null;
 		if (element.in_reply_to_status_id_str) {
 			let tweetId =
