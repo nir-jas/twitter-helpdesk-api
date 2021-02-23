537d0d31536984bfc250b85f548d81f74e1c68e3
Nirmal Jasmatiya <nirmal@Nirmals-MacBook-Pro.local>
2021-02-23 12:21:09 +0530

    WS Reply Channel Topic Updated

--- app/Listeners/Tweet.js
+++ app/Listeners/Tweet.js
@@ -10,14 +10,14 @@ Tweet.created = async (event) => {
 		let topic = null;
 		if (element.in_reply_to_status_id_str) {
 			topic = await Ws.getChannel("twitter:*").topic(
-				`twitter:${element.in_reply_to_status_id_str}`
+				`twitter:${element.conversation_id}`
 			);
 			if (topic) {
 				topic.broadcast("new:tweet:reply", element);
 			}
 		} else {
 			topic = await Ws.getChannel("twitter:*").topic(
-				`twitter:${element.conversation_id}`
+				`twitter:${element.in_reply_to_screen_name}`
 			);
 			if (topic) {
 				topic.broadcast("new:tweet", element);
